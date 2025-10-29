fdb9888018ab53223f8b8ffb0f25fed2e3b9e59e
Fidel <fidelpro@gmail.com>
2025-10-29 15:58:46 -0600

    terminé calculadora

--- src/app/components/calculadora/calculadora.ts
+++ src/app/components/calculadora/calculadora.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-calculadora',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, RouterLink],
   templateUrl: './calculadora.html',
-  styleUrl: './calculadora.css',
+  styleUrls: ['./calculadora.css']
 })
 export class Calculadora {
+  a: number = 0;
+  b: number = 0;
+  res: number = 0;
 
+  sumar() {
+    this.res = this.a + this.b;
+  }
+
+  restar() {
+    this.res = this.a - this.b;
+  }
+
+  multiplicar() {
+    this.res = this.a * this.b;
+  }
+
+  dividir() {
+    this.res = this.a / this.b;
+  }
 }
