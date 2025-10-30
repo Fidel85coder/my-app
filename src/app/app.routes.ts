f3c4c781cf5a8a82b52907dce990f0418482a5b3
Fidel <fidelpro@gmail.com>
2025-10-29 20:04:05 -0600

    creación del componente para imagen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Lista } from './components/lista/lista';
 import { Calculadora } from './components/calculadora/calculadora'
+import { ImagenComponent } from './components/imagen-component/imagen-component'
 
 export const routes: Routes = [
     { path: '', component: Lista },
     { path: 'calculadora', component: Calculadora },
+    { path: 'imagen', component: ImagenComponent },
 ];
---
+++ src/app/components/imagen-component/imagen-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imagen-component',
+  imports: [],
+  templateUrl: './imagen-component.html',
+  styleUrl: './imagen-component.css',
+})
+export class ImagenComponent {
+
+}
