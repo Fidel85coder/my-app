562569f6d0954d27428ff3b0c3517d82850c26ee
Fidel <fidelpro@gmail.com>
2025-10-29 20:30:40 -0600

    Lógica y diseño terminado para el componente imágenes

--- src/app/components/imagen-component/imagen-component.ts
+++ src/app/components/imagen-component/imagen-component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-imagen-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './imagen-component.html',
   styleUrl: './imagen-component.css',
 })
 export class ImagenComponent {
+  urlBase: string = "https://picsum.photos/id/";
+  url: string = "https://picsum.photos/id/237/400/300";
+  id: number | null = null;
 
+  modificarUrl(): void {
+    if (this.id !== null) {
+      this.url = this.urlBase + this.id + "/400/300";
+      // console.log(this.url);
+    }
+  }
 }
